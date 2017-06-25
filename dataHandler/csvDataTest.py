c4cbbeb168fb76ad64b4141ff37b1e62cff173a1
msantoro <msantoro@gmail.com>
2017-06-25 12:29:36 -0700

    Adding option chain support to CSV reader

--- dataHandler/csvDataTest.py
+++ dataHandler/csvDataTest.py
@@ -1,25 +1,34 @@
 import unittest
 import csvData
 import Queue as queue
+import pandas as pd
 
 class TestCSVHandler(unittest.TestCase):
+
+    # Used to set up the unittest class
+    def setUp(self):
+        # Create CsvData class object
+        self.dataProvider = 'iVolatility'
+        self.directory = './sampleData'
+        self.filename = 'aapl_sample_ivolatility.csv'
+        self.eventQueue = queue.Queue()
+        self.csvObj = csvData.CsvData(self.directory, self.filename, self.dataProvider, self.eventQueue)
 
     def testCSVLoad(self):
 
         #Load sample CSV file from sampleData directory into
         #pandas dataframe
-        dataProvider = 'iVolatility'
-        directory = './sampleData'
-        filename = 'aapl_sample_ivolatility.csv'
-        eventQueue = queue.Queue()
-
-        csvObj = csvData.CsvData(directory, filename, dataProvider, eventQueue)
 
         #Get row of CSV data
-        csvObj.getNextTick()
-        event = eventQueue.get()
+        self.csvObj.getNextTick()
+        event = self.eventQueue.get()
         dataObj = event.getData()
         self.assertEqual(dataObj.getUnderlyingPrice(), 94.48)
+
+    def testGetOptionChain(self):
+        #Get the option chain for each time/date from CSV
+        self.csvObj.getOptionChain()
+
 
 if __name__ == '__main__':
     unittest.main()
