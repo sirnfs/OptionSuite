62cf4aa2453f7427c69103b3d5e0728e78f8a577
msantoro <msantoro@gmail.com>
2017-07-03 14:51:30 -0700

    All unit tests working; rename event folder to events to prevent conflict with imports

--- dataHandler/csvDataTest.py
+++ dataHandler/csvDataTest.py
@@ -1,7 +1,7 @@
 import unittest
 import csvData
 import Queue as queue
-import pandas as pd
+import datetime
 
 class TestCSVHandler(unittest.TestCase):
 
@@ -14,21 +14,47 @@ class TestCSVHandler(unittest.TestCase):
         self.eventQueue = queue.Queue()
         self.csvObj = csvData.CsvData(self.directory, self.filename, self.dataProvider, self.eventQueue)
 
-    def testCSVLoad(self):
-
-        #Load sample CSV file from sampleData directory into
-        #pandas dataframe
-
-        #Get row of CSV data
-        self.csvObj.getNextTick()
-        event = self.eventQueue.get()
-        dataObj = event.getData()
-        self.assertEqual(dataObj.getUnderlyingPrice(), 94.48)
-
     def testGetOptionChain(self):
-        #Get the option chain for each time/date from CSV
+        #Get option chain for the first time/date from CSV
         self.csvObj.getOptionChain()
 
+        #Get event from the queue
+        event = self.eventQueue.get()
+        data = event.getData()
+
+        #Check that we got the right number of rows from the csv.
+        self.assertEqual(len(data), 1822)
+
+        #Get the first object of type put/call and do some basic checks
+        firstOption = data[0]
+
+        underlyingTicker = firstOption.getUnderlyingTicker()
+        self.assertEqual(underlyingTicker, 'AAPL')
+
+        optionType = firstOption.getOptionType()
+        self.assertEqual(optionType, 'CALL')
+
+        curDateTime = firstOption.getDateTime()
+        self.assertEqual(curDateTime, datetime.datetime.strptime('8/7/14', "%m/%d/%y"))
+
+        #Get option chain for the second date/time from CSV
+        self.csvObj.getOptionChain()
+
+        # Get event from the queue
+        event = self.eventQueue.get()
+        data = event.getData()
+
+        # Get the first object of type put/call and do some basic checks
+        firstOption = data[0]
+
+        underlyingTicker = firstOption.getUnderlyingTicker()
+        self.assertEqual(underlyingTicker, 'SPY')
+
+        optionType = firstOption.getOptionType()
+        self.assertEqual(optionType, 'CALL')
+
+        curDateTime = firstOption.getDateTime()
+        self.assertEqual(curDateTime, datetime.datetime.strptime('8/8/14', "%m/%d/%y"))
 
 if __name__ == '__main__':
     unittest.main()
---
+++ events/tickEventTest.py
@@ -0,0 +1,22 @@
+import unittest
+import tickEvent
+
+class TestTickEvent(unittest.TestCase):
+    """
+    To test the tick event creation, we will test that event
+    creation works, and that the type of the event is correct
+    """
+
+    def testCreateTickEvent(self):
+
+        #Create tick event
+        tickObj = tickEvent.TickEvent()
+
+        #Check that the data reference attribute is set to none
+        self.assertEqual(tickObj.getData(), None)
+
+        #Check that right type if set
+        self.assertEqual(tickObj.type, 'TICK')
+
+if __name__ == '__main__':
+    unittest.main()
